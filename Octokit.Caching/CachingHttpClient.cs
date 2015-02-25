c4be263bc799fae2310ca389fa6eb2f65f6845db
cordis-dev <cordis.dev@gmail.com>
2015-02-25 09:55:04 +0200

    Update CachingHttpClient.cs

--- Octokit.Caching/CachingHttpClient.cs
+++ Octokit.Caching/CachingHttpClient.cs
@@ -56,44 +56,5 @@ namespace Octokit.Caching
 
             return response;
         }
-		
-		 public async Task<IResponse<T>> SendV2<T>(IRequest request, CancellationToken cancellationToken)
-        {
-            if (request.Method != HttpMethod.Get)
-                return await httpClient.Send<T>(request, cancellationToken);
-
-            var key = request.Endpoint.ToString();
-
-            var response = await cache.GetAsync<IResponse<T>>(key);
-
-            if (response == null)
-            {
-                response = await httpClient.Send<T>(request, cancellationToken);
-
-                await cache.SetAsync(key, response);
-
-                return response;
-            }
-
-            if (!String.IsNullOrEmpty(response.ApiInfo.Etag))
-            {
-                request.Headers["If-None-Match"] = String.Format("{0}", response.ApiInfo.Etag);
-
-                var conditionalResponse = await httpClient.Send<T>(request, cancellationToken);
-
-                if (conditionalResponse.StatusCode == HttpStatusCode.NotModified)
-                    return response;
-
-                await cache.SetAsync(key, conditionalResponse);
-
-                return conditionalResponse;
-            }
-
-            response = await httpClient.Send<T>(request, cancellationToken);
-
-            await cache.SetAsync(key, response);
-
-            return response;
-        }
     }
 }
